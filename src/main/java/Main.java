21ceed8ff0ac85822371519904323b425bf86faf
Jack Cook <hello@jackcook.nyc>
2017-01-30 10:29:57 -0500

    Implemented an endpoint to retrieve categories from MySQL database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,67 @@
+import spark.Request;
+
+import java.sql.*;
+import java.util.ArrayList;
+
 import static spark.Spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World!");
-        // base url: http://localhost:4567
+        get("/categories", (req, res) -> retrieveCategories(req), new JsonTransformer());
+        System.out.println("Base URL: http://localhost:4567");
+    }
+
+    private static Category[] retrieveCategories(Request request) {
+        Statement statement = null;
+        ResultSet resultset = null;
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/cityhub?user=root&password=cityhub");
+
+            statement = connection.createStatement();
+            resultset = statement.executeQuery("SElECT * FROM categories");
+
+            ArrayList<Category> categories = new ArrayList<Category>();
+
+            while (resultset.next()) {
+                int id = resultset.getInt(1);
+                String name = resultset.getString(2);
+
+                Category category = new Category(id, name);
+                categories.add(category);
+            }
+
+            Category[] categoriesArray = new Category[categories.size()];
+            categoriesArray = categories.toArray(categoriesArray);
+
+            return categoriesArray;
+        } catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        } finally {
+            if (resultset != null) {
+                try {
+                    resultset.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                resultset = null;
+            }
+
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+                statement = null;
+            }
+        }
+
+        return null;
     }
 }
