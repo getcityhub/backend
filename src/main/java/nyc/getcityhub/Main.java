9e7f77114f043a7f306dfb953e72ae9cc6697f4a
Carol Shao <carolshao630@gmail.com>
2017-02-12 13:02:54 -0500

    updated main with logging info

--- src/main/java/nyc/getcityhub/Main.java
+++ src/main/java/nyc/getcityhub/Main.java
@@ -3,19 +3,37 @@ package nyc.getcityhub;
 import nyc.getcityhub.controllers.CategoryController;
 import nyc.getcityhub.controllers.PostController;
 import nyc.getcityhub.controllers.UserController;
+import org.eclipse.jetty.server.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 import static spark.Spark.*;
 
 public class Main {
 
+    final static Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
         before((request, response) -> response.type("application/json; charset=utf-8"));
+
+        before((request, response) -> {
+            DateFormat df = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
+            Date date = Calendar.getInstance().getTime();
+            String reportDate = df.format(date);
+            logger.info(request.ip() + " [" + reportDate + "] \"" + request.requestMethod() + " " + request.uri() + " " + request.protocol() + "\"");
+            //need to add response status code later
+        });
 
         post("/posts", (req, res) -> PostController.createPost(req), new JsonTransformer());
         post("/users", (req, res) -> UserController.createUser(req), new JsonTransformer());
 
         get("/categories", (req, res) -> CategoryController.retrieveCategories(req), new JsonTransformer());
-        get("/posts",(req, res) -> PostController.retrievePosts(req), new JsonTransformer());
+        get("/posts", (req, res) -> PostController.retrievePosts(req), new JsonTransformer());
 
         exception(BadRequestException.class, (exception, request, response) -> {
             ResponseError error = new ResponseError(400, exception.getMessage());
@@ -33,6 +51,6 @@ public class Main {
             response.body(transformer.render(error));
         });
 
-        System.out.println("Base URL: http://localhost:4567");
+        logger.info("Base URL: http://localhost:4567");
     }
 }
