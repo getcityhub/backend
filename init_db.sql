6c64e6eae654f4354b078c334168a1d36284e4f8
Jack Cook <hello@jackcook.nyc>
2017-03-04 08:35:35 -0500

    Add translations to DB init script

--- init_db.sql
+++ init_db.sql
@@ -59,3 +59,16 @@ CREATE TABLE translations (
   simplified TEXT,
   traditional TEXT
 );
+
+INSERT INTO translations VALUES ('Democrat', 'Demócrata', 'Démocrate', '民主党', '民主黨'),
+    ('Republican', 'Republicano', 'Républicain', '共和党', '共和黨'),
+    ('President', 'Presidente', 'Président', '总统', '總統'),
+    ('Vice President', 'Vicepresidente', 'Vice-Président', '副总统', '副總統'),
+    ('Senator', 'Senador', 'Sénateur', '参议员', '參議員'),
+    ('Governor', 'Gobernador', 'Gouverneur', '州长', '州長'),
+    ('Lieutenant Governor', 'Vicegobernador', 'Lieutenant Gouverneur', '副州长', '副州長'),
+    ('Mayor', 'Alcalde', 'Maire', '市长', '市長'),
+    ('Comptroller', 'Contraloría', 'Contrôleur', '审计官', '審計官'),
+    ('Public Advocate', 'Defensor Publico', 'Défenseur public', '公众倡议者', '公眾倡議者'),
+    ('Attorney General', 'Fiscal general', 'Avocat général', '司法部长', '司法部長'),
+    ('State Comptroller', 'Anditor del estado', 'Contrôleur d\'état', '州审计长', '州審計長');
